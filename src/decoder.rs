18bc9f010ca9b6defe3e99dc39763d2ef56b9213
h0mtanks <gurnoorsingh3@gmail.com>
2023-03-11 14:58:42 +0530

    immediate to memory explicit sizes

--- src/decoder.rs
+++ src/decoder.rs
@@ -43,19 +43,18 @@ pub fn decode_mov(
     let second_byte = instructions[offset + 1];
     let mut num_bytes_in_instruction = 2;
 
+    //* Extract instruction fields
+    let direction = (first_byte & 0b00000010) > 0;
+    let word = (first_byte & 0b00000001) > 0;
+    let rm = second_byte & 0b00000111;
+    let reg = (second_byte & 0b00111000) >> 3;
+    let mode = (second_byte & 0b11000000) >> 6;
+
     //* Register/Memory to/from register
     {
         //* Test 6 bit opcode
         let opcode = 0b100010;
         if first_byte >> 2 == opcode {
-            //* Extract instruction fields
-            let direction = (first_byte & 0b00000010) > 0;
-            let word = (first_byte & 0b00000001) > 0;
-
-            let rm = second_byte & 0b00000111;
-            let reg = (second_byte & 0b00111000) >> 3;
-            let mode = (second_byte & 0b11000000) >> 6;
-
             if mode == 0b11 {
                 //* Register to register mode
 
@@ -70,7 +69,7 @@ pub fn decode_mov(
             } else {
                 //* Memory to register mode, where mode = 00 | 01 | 10
                 if mode == 0b00 && rm == 0b110 {
-                    //*Direct address case */
+                    //TODO Direct address case
                 } else {
                     let mut source_addr_str = String::new();
                     write!(source_addr_str, "{}", MEM_ADDR_MODE_MAPPING[rm as usize])?;
@@ -79,13 +78,13 @@ pub fn decode_mov(
                     //* by checking the mode != 0b00
                     //? Displacements are signed even though the manual says unsigned?
                     if mode != 0b00 {
-                        let third_byte = instructions[offset + 2];
+                        let third_byte = instructions[offset + num_bytes_in_instruction];
                         num_bytes_in_instruction += 1;
                         let mut disp = third_byte as i16;
 
                         //* Check for 16bit displacement
                         if mode == 0b10 {
-                            let fourth_byte = instructions[offset + 3];
+                            let fourth_byte = instructions[offset + num_bytes_in_instruction];
                             num_bytes_in_instruction += 1;
                             disp = i16::from_le_bytes([third_byte, fourth_byte]);
                         }
@@ -120,7 +119,7 @@ pub fn decode_mov(
                 }
             }
 
-            return Ok(num_bytes_in_instruction);
+            return Ok(num_bytes_in_instruction as u8);
         }
     }
 
@@ -133,14 +132,65 @@ pub fn decode_mov(
 
             let mut data = second_byte as u16;
             if word {
-                let third_byte = instructions[offset + 2];
+                let third_byte = instructions[offset + num_bytes_in_instruction];
                 data = u16::from_le_bytes([second_byte, third_byte]);
                 num_bytes_in_instruction += 1;
             }
 
             writeln!(output, "mov {}, {}", get_register_name(reg, word), data)?;
 
-            return Ok(num_bytes_in_instruction);
+            return Ok(num_bytes_in_instruction as u8);
+        }
+    }
+
+    //* Immediate to memory
+    {
+        let opcode = 0b1100011;
+        if (first_byte >> 1) == opcode && reg == 0b000 {
+            let mut source_addr_str = String::new();
+            write!(source_addr_str, "{}", MEM_ADDR_MODE_MAPPING[rm as usize])?;
+
+            //* Check if addr should have displacement
+            //* by checking the mode != 0b00
+            //? Displacements are signed even though the manual says unsigned?
+            if mode != 0b00 {
+                let third_byte = instructions[offset + num_bytes_in_instruction];
+                num_bytes_in_instruction += 1;
+                let mut disp = third_byte as i16;
+
+                //* Check for 16bit displacement
+                if mode == 0b10 {
+                    let fourth_byte = instructions[offset + num_bytes_in_instruction];
+                    num_bytes_in_instruction += 1;
+                    disp = i16::from_le_bytes([third_byte, fourth_byte]);
+                }
+
+                //* No need to print displacement if it's 0
+                if disp != 0 {
+                    if mode == 0b01 {
+                        //* 8 bit displacement
+                        write!(source_addr_str, " + {}", disp as i8)?;
+                    } else {
+                        //* 16 bit displacement
+                        write!(source_addr_str, " + {}", disp)?;
+                    }
+                }
+            }
+
+            let fifth_byte = instructions[offset + num_bytes_in_instruction];
+            num_bytes_in_instruction += 1;
+            let mut data = fifth_byte as u16;
+            //*Check if word or byte operation
+            if word {
+                let sixth_byte = instructions[offset + num_bytes_in_instruction];
+                num_bytes_in_instruction += 1;
+                data = u16::from_le_bytes([fifth_byte, sixth_byte]);
+                writeln!(output, "mov [{}], word {}", source_addr_str, data)?;
+            } else {
+                writeln!(output, "mov [{}], byte {}", source_addr_str, data)?;
+            }
+
+            return Ok(num_bytes_in_instruction as u8);
         }
     }
 
